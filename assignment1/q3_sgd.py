Correct negative sampling cost and gradient

assignment1/q3_sgd.py
@@ -85,7 +85,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         cost = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+        cost, grad = f(x)
+        x -= grad * step
+        x = postprocessing(x)
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
@@ -105,6 +107,7 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
 
 def sanity_check():
+    # Minimum of x**2 is x=0, expect result to be close to 0
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
     print "Running sanity checks..."
@@ -132,7 +135,25 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    # Minimum of x**2 - 12*x is x=6
+    quad = lambda x: (np.sum(x**2 - 12*x), 2*x - 12)
+
+    def in_range(x, value, eps):
+        return (x <= value + eps) & (x >= value - eps)
+
+    t1 = sgd(quad, 0.5, 0.01, 1000, PRINT_EVERY=100)
+    print "test 1 result:", t1
+    assert in_range(t1, 6, 1e-6)
+
+    t2 = sgd(quad, 0.0, 0.01, 1000, PRINT_EVERY=100)
+    print "test 2 result:", t2
+    assert in_range(t2, 6, 1e-6)
+
+    t3 = sgd(quad, -1.5, 0.01, 1000, PRINT_EVERY=100)
+    print "test 3 result:", t3
+    assert in_range(t3, 6, 1e-6)
+
+    print ""
     ### END YOUR CODE
 
 

assignment1/q3_word2vec.py
@@ -110,17 +110,19 @@ def negSamplingCostAndGradient(predicted, target, outputVectors, dataset,
     indices.extend(getNegativeSamples(target, dataset, K))
 
     ### YOUR CODE HERE
-    # Calculate cost and gradients using subset of outputVectors
-    neg_indices = indices[1:] # exclude the target index
-    cost, gradPred, neg_samples_grad = softmaxCostAndGradient(predicted,
-            target,
-            outputVectors[neg_indices],
-            dataset)
+    sign = -np.ones((len(indices), 1)) # (K+1,1)
+    sign[0] = 1
+    predicted = predicted[:, np.newaxis] # (D,1)
+    sig_scores = sigmoid(np.dot(sign * outputVectors[indices], predicted)) # (K+1,1)
+
+    cost = -np.sum(np.log(sig_scores))
+    
+    gradPred = np.sum(-sign * (1 - sig_scores) * outputVectors[indices], axis=0)
 
     # Assign gradients to corresponding word vectors
     grad = np.zeros(outputVectors.shape)
-    for k in range(K):
-        grad[neg_indices[k]] += neg_samples_grad[k]
+    for i in range(len(indices)):
+        grad[indices[i]] += -sign[i] * (1 - sig_scores[i]) * predicted.flatten()
     ### END YOUR CODE
 
     return cost, gradPred, grad
@@ -158,16 +160,16 @@ def skipgram(currentWord, C, contextWords, tokens, inputVectors, outputVectors,
     pred_ind = tokens[currentWord]
     predicted = inputVectors[pred_ind]
 
-    for i in range(len(contextWords)):
-        context_ind = tokens[contextWords[i]]
-        cost_temp, gradPred_temp, grad_temp = word2vecCostAndGradient(predicted,
+    for w in contextWords:
+        context_ind = tokens[w]
+        cost_temp, gradPred, grad = word2vecCostAndGradient(predicted,
                 context_ind,
                 outputVectors,
                 dataset)
 
         cost += cost_temp
-        gradIn[pred_ind] += gradPred_temp
-        gradOut += grad_temp
+        gradIn[pred_ind] += gradPred
+        gradOut += grad
     ### END YOUR CODE
 
     return cost, gradIn, gradOut